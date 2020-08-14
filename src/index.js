cd3901cd287cb9c90186f02575082ed680c05b75
xiaowensheng <jian958753831@qq.com>
2020-08-14 17:36:06 +0800

    判断参数

--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ const debug = require('debug')
 const dotenv = require('dotenv')
 const dotenvExpand = require('dotenv-expand')
 
-module.exports = function loadEnv(mode, { cwd } = { cwd: process.cwd() }) {
+function isObject(value) {
+    return Object.prototype.toString.call(value) === '[object Object]'
+}
+
+module.exports = function loadEnv(mode, options) {
+    if (isObject(mode)) {
+        options = mode
+        mode = ''
+    }
+
+    const { cwd } = Object.assign({}, { cwd: process.cwd() }, options || {})
+
     const logger = debug('vue:env')
     const basePath = path.resolve(cwd, `.env${mode ? `.${mode}` : ``}`)
     const localPath = `${basePath}.local`
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,5 +1,23 @@
 const lib = require('../src')
+const mode = 'development'
+if (mode) {
+    lib('development', { cwd: __dirname })
+}
 
-lib('development', { cwd: __dirname })
+lib({ cwd: __dirname })
 
-console.log(process.env)
+Object.prototype.filter = function (re) {
+    const that = this
+
+    return Object.keys(that)
+        .filter((key) => {
+            return re.exec(key)
+        })
+        .reduce((prev, cur) => {
+            prev[cur] = that[cur]
+
+            return prev
+        }, {})
+}
+
+console.log(process.env.filter(/^MONGOLAB/i))
